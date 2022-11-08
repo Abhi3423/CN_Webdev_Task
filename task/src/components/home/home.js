4cbfe9fe296535575eb2d689a3363816c4b8df05
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-11-09 01:38:09 +0530

    2 sections completed

--- task/src/components/home/home.js
+++ task/src/components/home/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./nav.css";
 import "./geometry.css";
 import "./home.css";
+import Section2 from '../section2/section2';
 
 function home() {
   return (
@@ -13,7 +14,7 @@ function home() {
         <a href="/" id="link" style={{"left":"70.28%","right":"26.39%"}}>Link 2</a>
         <a href="/" id="link" style={{"left":"75.83%","right":"20.83%"}}>Link 3</a>
         <a href="/" id="link" style={{"left":"81.39%","right":"15.28%"}}>Link 4</a>
-        <button id="signup"> <p style={{"font-size":"2.5vh","margin-top":"5.5%"}}>Sign Up </p></button>
+        <button id="signup"> <p style={{"font-size":"1.25vw","textAlign":"center","margin-top":"5.5%"}}>Sign Up </p></button>
       </div>
 
       <div id="box1"></div>
@@ -23,9 +24,15 @@ function home() {
 
 
 
+      <div id="box2">
+      <p id="head_content">CN RECRUITMENT</p>
+      <span id="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+      <button type="button" id="start" href="/" className="btn btn-dark"><span style={{"font-size":"1vw"}}>Start free trial</span></button>
+      </div>
+
 
       <div id="box3">
-        <p id="content">Trusted by the World’s Best Companies</p>
+        <p id="foot_content">Trusted by the World’s Best Companies</p>
         <div id="circle" style={{"left":"68.06%","right":"26.39%"}}></div>
         <div id="circle" style={{"left":"59.72%","right":"34.72%"}}></div>
         <div id="circle" style={{"left":"51.39%","right":"43.06%"}}></div>
@@ -33,6 +40,8 @@ function home() {
         <div id="circle" style={{"left":"34.72%","right":"59.72%"}}></div>
         <div id="circle" style={{"left":"26.39%","right":"68.06%"}}></div>
       </div>
+
+      <Section2></Section2>
     </div>
     </>
   );
---
+++ task/src/components/section2/section2.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './section2.css'
+
+function Section2() {
+  return (
+    <>
+      <div id="box">
+        <div id="s2box1">
+          <p id="s2head_content">CN RECRUITMENT</p>
+          <span id="s2content1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+          <span id="s2content2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+          <div id="s2dot"></div>
+          <span id="s2content3">Dhaka Oke, Product Designer</span>
+        </div>
+
+        <div id="s2box3"></div>
+        <div id="s2box2">
+          <div id="s2squ"></div>
+          <div id="s2tri"></div>
+          <div id="s2line"></div>
+          <div id="s2dot2"></div>
+        </div>
+      </div>
+    </>
+  );
+
+};
+
+export default Section2;
