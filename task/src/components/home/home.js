a5c0993b84c5f1c137b3e04bec5b559f1b54f005
Abhishek Yadav <abhishekaug2002@gmail.com>
2022-11-10 04:16:04 +0530

    cursor and minor changes updated

--- task/src/components/home/home.js
+++ task/src/components/home/home.js
@@ -15,38 +15,34 @@ function Home() {
     ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
-
   return (
     <>
       <div>
+        
         <div className="navbar">
-          <p id="head">CN RECRUITMENT</p>
+          <span id="head">CN RECRUITMENT</span>
           <a href="/" id="link" style={{ "left": "64.72%", "right": "31.94%" }}>Link 1</a>
           <a href="/" id="link" style={{ "left": "70.28%", "right": "26.39%" }}>Link 2</a>
           <a href="/" id="link" style={{ "left": "75.83%", "right": "20.83%" }}>Link 3</a>
           <a href="/" id="link" style={{ "left": "81.39%", "right": "15.28%" }}>Link 4</a>
-          <button id="signup"> <p style={{ "fontSize": "1.25vw", "textAlign": "center", "marginTop": "5.5%" }}>Sign Up </p></button>
+          <button id="signup"> <p style={{ "fontSize": "1.25vw", "textAlign": "center" }}>Sign Up </p></button>
         </div>
 
         <div id="box1">
-          <div id="box1dot1" style={{"left":"3%","top":"2.5%"}}></div>
-          <div id="box1dot1" style={{"left":"6%","top":"2.5%"}}></div>
-          <div id="box1dot1" style={{"left":"9%","top":"2.5%"}}></div>
+          <div id="box1dot1" style={{ "left": "3%", "top": "2.5%" }}></div>
+          <div id="box1dot1" style={{ "left": "6%", "top": "2.5%" }}></div>
+          <div id="box1dot1" style={{ "left": "9%", "top": "2.5%" }}></div>
           <div id="box1squ"></div>
           <div id="box1tri1"></div>
           <div id="box1tri2"></div>
           <div id="box1tri3"></div>
           <div id="box1circle1"></div>
           <div id="box1circle2"></div>
-          <div id="box1dot2" style={{"left":"74%","top":"7vw"}}></div>
-          <div id="box1dot2" style={{"left":"21%","top":"13.5vw"}}></div>
+          <div id="box1dot2" style={{ "left": "74%", "top": "7vw" }}></div>
+          <div id="box1dot2" style={{ "left": "21%", "top": "13.5vw" }}></div>
           <div id="box1line1"></div>
           <div id="box1line2"></div>
         </div>
-
-
-
-
 
 
         <div id="box2">
@@ -67,9 +63,9 @@ function Home() {
         </div>
 
         <svg className="arrows" onClick={handleClick} >
-              <path className="a1" d="M0 0 L30 32 L60 0"></path>
-              <path className="a2" d="M0 20 L30 52 L60 20"></path>
-              <path className="a3" d="M0 40 L30 72 L60 40"></path>
+          <path className="a1" d="M0 0 L30 32 L60 0"></path>
+          <path className="a2" d="M0 20 L30 52 L60 20"></path>
+          <path className="a3" d="M0 40 L30 72 L60 40"></path>
         </svg>
 
         <Section2></Section2>
